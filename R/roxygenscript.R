e3c9d60eb08d61bb4f6d50794d989681750fb6e1
jdickinson267 <jldicki2@asu.edu>
2025-09-19 17:10:16 -0500

    Update roxygenscript.R

--- R/roxygenscript.R
+++ R/roxygenscript.R
@@ -35,11 +35,26 @@ create_game <- function()
 
 
 #' @title
+#' Contestant Selects A Door
+#'
 #' @description
+#' 'select_door' function sets up the possible door selections and makes a
+#' selection at random.
+#'
 #' @details
+#' This step in the game is when a contestant sees the amount of doors in the
+#' game and chooses a door at random.  The function will return the door selection.
+#'
 #' @param
+#' No arguments used in this function.
+#'
 #' @return
+#' The function returns a length 1 character vector indicating the position of
+#' the chosen door.
+#'
 #' @examples
+#' select_door()
+#'
 #' @export
 select_door <- function( )
 {
@@ -51,11 +66,29 @@ select_door <- function( )
 
 
 #' @title
+#' Host Opens Goat Door
+#'
 #' @description
+#''open_goat_door' function provides the position of one of the doors with a goat.
+#'
 #' @details
+#' This step in the game is when the host opens one of the doors that is not a
+#' winning door, reducing the remaining choice of doors for the contestant to
+#' choose from.
+#'
 #' @param
+#' The parameters to this function include the amount of doors and the position
+#' of the cars and goats in the game.
+#'
 #' @return
+#' The function returns a length 1 character vector indicating the position of one
+#' of the goats in the game.
+#'
 #' @examples
+#' this.game <- c("goat","car","goat")
+#' my.initial.pick <- 1
+#' open_goat_door( this.game, my.initial.pick ) 
+#'
 #' @export
 open_goat_door <- function( game, a.pick )
 {
@@ -77,11 +110,26 @@ open_goat_door <- function( game, a.pick )
 
 
 #' @title
+#' Change Doors
+#'
 #' @description
+#' 'change_door' function allows the contestant the option to change their choice
+#' in doors when they have already opened one of the losing doors.
+#'
 #' @details
+#' This step in the game is when the contestant has to decide to stay or switch to
+#' a new door choice before the final prize reveal.
+#'
 #' @param
+#' The parameters to this function include the amount of doors in the game and whether
+#' or not the contestant chooses to keep their current door choice.
+#'
 #' @return
+#' The function returns a length 1 character vector indicating the door choice.
+#'
 #' @examples
+#' change_door( stay=T, opened.door=1, a.pick=3 ) 
+#'
 #' @export
 change_door <- function( stay=T, opened.door, a.pick )
 {
@@ -102,11 +150,27 @@ change_door <- function( stay=T, opened.door, a.pick )
 
 
 #' @title
+#' Determine if Contestant Has Won
+#'
 #' @description
+#' 'determine_winner' function returns whether or not the final door choice is
+#' the position of the car.
+#'
 #' @details
+#' This step in the game is the final reveal of the doors and whether the contestant
+#' has won the car or not.
+#'
 #' @param
+#' The parameters to this function include the previous steps being completed to provide
+#' the game's results.
+#'
 #' @return
+#' The function returns a character string of "LOSE" or "WIN".
+#'
 #' @examples
+#' this.game <- c("goat","car","goat")
+#'determine_winner( final.pick=1, game=this.game )
+#'
 #' @export
 determine_winner <- function( final.pick, game )
 {
@@ -125,11 +189,25 @@ determine_winner <- function( final.pick, game )
 
 
 #' @title
+#' Full Game Test
+#'
 #' @description
+#' 'play_game' function combines all of the previous steps into one function to play
+#' a full game and provide the resulting outcomes.
+#'
 #' @details
+#' This would be a full game played without individual steps, providing a table of the strategy
+#' and the resulting outcome based on that choice.
+#'
 #' @param
+#' No arguments used in this function.
+#'
 #' @return
+#' The function returns a table of the strategies and the results of either "LOSE" or "WIN".
+#'
 #' @examples
+#' play_game( )
+#'
 #' @export
 play_game <- function( )
 {
@@ -156,11 +234,26 @@ play_game <- function( )
 
 
 #' @title
+#' Adding the Game to a Loop
+#'
 #' @description
+#' "play_n_games' function plays a certain number of games and provides the
+#' probability of how likely the strategy will result in each outcome.
+#'
 #' @details
+#' This would be a full game played without individual steps, providing a table of the strategy
+#' and the resulting probability of each outcome based on that choice.
+#'
 #' @param
+#' The parameters to this function include the previous steps being completed to provide
+#' the game's probability of each result when ran n times.
+#'
 #' @return
+#' The function returns a table of the strategies and the probability of either "LOSE" or "WIN".
+#'
 #' @examples
+#' play_n_games( )
+#'
 #' @export
 play_n_games <- function( n=100 )
 {
